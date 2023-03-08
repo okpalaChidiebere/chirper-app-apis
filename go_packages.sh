8ca9e6e74880b621002a44241d2be1a903f8163d
okpalaChidiebere <okpalacollins4@gmail.com>
2023-03-08 10:12:06 -0600

    chore

--- go_packages.sh
+++ go_packages.sh
@@ -8,12 +8,17 @@ for f in $(find gen/go -maxdepth 1 -mindepth 1 -type d); do
     # gen/go
     # d=$(dirname $f)
 
-    (
+    # (
+    #     cd $f; 
+    #     sudo touch go.mod go.sum; 
+    #     sudo chmod -R 777 go.mod go.sum; 
+    #     echo "module github.com/${USER}/${GEN_REPO_GO}/${b}" > go.mod;
+    #     go mod tidy;
+    # )
+     (
         cd $f; 
-        sudo touch go.mod go.sum; 
-        sudo chmod -R 777 go.mod go.sum; 
-        echo "module github.com/${USER}/${GEN_REPO_GO}/${b}" > go.mod;
-        go mod tidy;
+        sudo go mod init "github.com/okpalaChidiebere/chirper-app-gen-protos/${b}"; 
+        sudo go mod tidy; 
     )
     # uncomment below for local testing what whatever reason
     # (cd $f; go mod init "github.com/okpalaChidiebere/chirper-app-gen-protos/${b}"; go mod tidy; rm -f go.sum)
